17b94480cc9e9a016b41030f595a828bc1c3c4be
Amund Engen Bogetvedt <amundeb@gmail.com>
2017-10-09 15:05:50 +0200

    Add project files.

---
+++ TravelVisualMan/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TravelVisualMan {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.Algorithm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Initial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InitialTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Improved = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ImprovedTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(12, 12);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Algorithm,
+            this.Initial,
+            this.InitialTime,
+            this.Improved,
+            this.ImprovedTime});
+            this.DataGridView.Location = new System.Drawing.Point(13, 53);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(544, 221);
+            this.DataGridView.TabIndex = 1;
+            this.DataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_CellContentClick);
+            // 
+            // Algorithm
+            // 
+            this.Algorithm.HeaderText = "Initial Algorithm";
+            this.Algorithm.Name = "Algorithm";
+            // 
+            // Initial
+            // 
+            this.Initial.HeaderText = "Initial Solution";
+            this.Initial.Name = "Initial";
+            this.Initial.ReadOnly = true;
+            // 
+            // InitialTime
+            // 
+            this.InitialTime.HeaderText = "Initial Time";
+            this.InitialTime.Name = "InitialTime";
+            this.InitialTime.ReadOnly = true;
+            // 
+            // Improved
+            // 
+            this.Improved.HeaderText = "Improved Solution";
+            this.Improved.Name = "Improved";
+            this.Improved.ReadOnly = true;
+            // 
+            // ImprovedTime
+            // 
+            this.ImprovedTime.HeaderText = "Improved Time";
+            this.ImprovedTime.Name = "ImprovedTime";
+            this.ImprovedTime.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 286);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.StartBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Algorithm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Initial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InitialTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Improved;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ImprovedTime;
+    }
+}
+
---
+++ TravelVisualMan/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelVisualMan {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+        // Method filling the the matrix with cities and distances
+        private int[,] MatrixFill(int nodes) {
+            Random rnd = new Random();
+            int edge;
+            int[,] cities = new int[nodes, nodes];
+
+            for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    if (cities[i, j] != 0)// If space allready is filled.
+                        continue;
+                    else if (j == i) { // Distance to the node itself is 0.
+                        cities[i, j] = 0;
+                    }
+                    else {
+                        edge = rnd.Next(1, 10);
+                        cities[i, j] = edge;
+                        cities[j, i] = edge;
+                    }
+                }
+            }
+            /*for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    Console.Write(cities[i,j] + " ");
+                    if (j == nodes-1)
+                        Console.WriteLine();
+                }
+            }*/
+            Console.WriteLine();
+            return cities;
+        }
+        // Method calculating total cost of a route
+        private int GetCost(int[] route, int[,] cities) {
+            int cost = 0;
+
+            for (int i = 0; i < route.Length - 1; i++) {
+                cost += cities[route[i], route[i + 1]];
+            }
+            cost += cities[route[route.Length - 1], route[0]];
+            return cost;
+        }
+        // Method making a random solution
+        private int[] Random(int numbOfCities, int[,] cities) {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            int i = 0;
+            Random rnd = new Random();
+            int[] route = new int[numbOfCities];
+            int[] exists = new int[numbOfCities];
+
+            while (i < numbOfCities) {
+                int rando = rnd.Next(numbOfCities);
+                if (exists[rando] == 1)
+                    continue;
+                else {
+                    route[i] = rando;
+                    exists[rando] = 1;
+                    i++;
+                }
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            DataGridView.Rows[Count].Cells[2].Value = elapsedMs;
+            return route;
+        }
+        // Making an iterative random solution
+        private int[] IterativeRandom(int numbOfCities, int[,] cities, int iterations) {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            int j, cost;
+            int minCost = Int32.MaxValue;
+            Random rnd = new Random();
+            int[] route, exists;
+            int[] cheapest = new int[numbOfCities];
+
+            for (int i = 0; i < iterations; i++) {
+                cost = 0;
+                j = 0;
+                exists = new int[numbOfCities];
+                route = new int[numbOfCities];
+                while (j < numbOfCities) {
+                    int rando = rnd.Next(numbOfCities);
+                    if (exists[rando] == 1)
+                        continue;
+                    else {
+                        route[j] = rando;
+                        exists[rando] = 1;
+                        j++;
+                    }
+                }
+                cost = GetCost(route, cities);
+
+                if (cost < minCost) {
+                    minCost = cost;
+                    Array.Copy(route, cheapest, numbOfCities);
+                }
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            DataGridView.Rows[Count].Cells[2].Value = elapsedMs;
+            return cheapest;
+        }
+        // Making a greedy solution
+        private int[] Greedy(int numbOfCities, int[,] cities) {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            int[] exists = new int[numbOfCities];
+            int[] route = new int[numbOfCities];
+            Random rnd = new Random();
+
+            int city = rnd.Next(numbOfCities);
+            exists[city] = 1;
+            route[0] = city;
+            for (int i = 1; i < numbOfCities; i++) {
+                int min = Int32.MaxValue;
+                for (int j = 0; j < numbOfCities; j++) {
+                    if (cities[route[i - 1], j] < min && cities[route[i - 1], j] != 0 && exists[j] != 1) {
+                        min = cities[route[i - 1], j];
+                        city = j;
+                    }
+                }
+                route[i] = city;
+                exists[city] = 1;
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            DataGridView.Rows[Count].Cells[2].Value = elapsedMs;
+            return route;
+        }
+        // Method improving the initial solutions
+        private int Improve(int[] initial, int[,] cities) {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            int cost = GetCost(initial, cities);
+            int temp, newCost;
+            int stagnation = 0;
+            int[] cheapest = new int[initial.Length];
+            bool stop = false;
+            Random rnd = new Random();
+
+            Array.Copy(initial, cheapest, initial.Length);
+            while (!stop) {
+                int city1 = rnd.Next(initial.Length);
+                int city2 = rnd.Next(initial.Length);
+
+                while (city1 == city2) {
+                    city2 = rnd.Next(initial.Length);
+                }
+
+                temp = initial[city1];
+                initial[city1] = initial[city2];
+                initial[city2] = temp;
+
+                newCost = GetCost(initial, cities);
+                if (newCost < cost) {
+                    Array.Copy(initial, cheapest, initial.Length);
+                    cost = newCost;
+                    stagnation = 0;
+                }
+                else {
+                    stagnation++;
+                }
+                if (stagnation > 1000) {
+                    stop = true;
+                }
+                Array.Copy(cheapest, initial, cheapest.Length);
+            }
+            //PrintRoute(cheapest);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            DataGridView.Rows[Count].Cells[4].Value = elapsedMs;
+            return cost;
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e) {
+            int ncities = 1000;
+            int [,] cities = MatrixFill(ncities);
+
+            for(int i = 0; i < 100; i++) {
+                RandomFill(ncities, cities);
+                IterativeRandomFill(ncities, cities);
+                GreedyFill(ncities, cities);
+            }
+        }
+
+        private void RandomFill(int ncities, int[,] cities) {
+            DataGridView.Rows.Add();
+            DataGridView.Rows[Count].Cells[0].Value = "Random";
+            int[] random = Random(ncities, cities);
+            int cost = GetCost(random, cities);
+            DataGridView.Rows[Count].Cells[1].Value = cost;
+            int imp = Improve(random, cities);
+            DataGridView.Rows[Count].Cells[3].Value = imp;
+        }
+
+        private void IterativeRandomFill(int ncities, int[,] cities) {
+            DataGridView.Rows.Add();
+            DataGridView.Rows[Count].Cells[0].Value = "Iterative Random";
+            int[] iterativeRandom = IterativeRandom(ncities, cities, 5);
+            int cost = GetCost(iterativeRandom, cities);
+            DataGridView.Rows[Count].Cells[1].Value = cost;
+            int imp = Improve(iterativeRandom, cities);
+            DataGridView.Rows[Count].Cells[3].Value = imp;
+        }
+
+        private void GreedyFill(int ncities, int[,] cities) {
+            DataGridView.Rows.Add();
+            DataGridView.Rows[Count].Cells[0].Value = "Greedy";
+            int[] greedy = Greedy(ncities, cities);
+            int cost = GetCost(greedy, cities);
+            DataGridView.Rows[Count].Cells[1].Value = cost;
+            int imp = Improve(greedy, cities);
+            DataGridView.Rows[Count].Cells[3].Value = imp;
+        }
+
+        private void DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private int Count {
+            get { return DataGridView.RowCount-2; }
+        }
+    }
+}
